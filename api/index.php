7f96ceb4281571adf15ecfbb9a2ec9cce2f20712
勿埋我心 <qiantigers@gmail.com>
2022-11-11 15:50:03 +0800

    update

---
+++ api/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 二维码生成接口
+ *
+ * 请求方式   http get
+ *
+ * 请求字段描述
+ * 参数    说明                  类型                                 是否必需
+ * c	content             二维码内容 如：http://www.demo.com	 	 是
+ * s	size                大小，每像素几个点 1,2,3,4,5,6....	     否 默认7
+ * bc	backgroud_color     背景色&透明度 rgba 逗号分开	 	     否 默认白色 255255,255,0
+ * fc	froreground_color   前景色&透明度 rgba 逗号分开	 	     否 默认黑色 0,0,0,0
+ *
+ * 返回字段描述
+ * 返回示例
+ * 直接 返回图片
+ *
+ * Date: 16/5/19
+ * Time: 10:52
+ */
+include_once('./QRcode.php');
+
+// 接收参数
+$content = $_GET['c'] ? $_GET['c'] : 'https://www.qian.blue';
+$size = $_GET['s'] ? $_GET['s'] : 7;
+$background = $_GET['bc']?explode(',',$_GET['bc']):array(255, 255, 255, 0);
+$foreground = $_GET['fc']?explode(',',$_GET['fc']):array(0, 0, 0, 0);
+
+// 图片输出
+header("Content-type: image/jpg");
+echo Toplib_Lib_QRcode::png($content,false,QR_ECLEVEL_L,$size,1,false,$background,$foreground);
---
+++ assets/js/build_url.js
@@ -0,0 +1,11 @@
+function build_url(){
+    var url = encodeURI(document.querySelector('#url').value);
+    if (url === "" || url.indexOf("http") === -1){
+        document.getElementById("b_url").innerHTML=`请输入链接！`;
+    }
+    else {
+        url = "api/?c=" + url;
+        document.getElementById("b_url").innerHTML=`<img src="${url}" width=200></img>`;
+    }
+
+}
