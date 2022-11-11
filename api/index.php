98641e5c9bf4494580d5528f8327801bd5099953
勿埋我心 <57932548+Qiantigers@users.noreply.github.com>
2022-11-11 17:38:46 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -10,13 +10,6 @@
  * s	size                大小，每像素几个点 1,2,3,4,5,6....	     否 默认7
  * bc	backgroud_color     背景色&透明度 rgba 逗号分开	 	     否 默认白色 255255,255,0
  * fc	froreground_color   前景色&透明度 rgba 逗号分开	 	     否 默认黑色 0,0,0,0
- *
- * 返回字段描述
- * 返回示例
- * 直接 返回图片
- *
- * Date: 16/5/19
- * Time: 10:52
  */
 include_once('./QRcode.php');
 
